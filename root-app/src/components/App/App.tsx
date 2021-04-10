5050aeddd82bb2397a02cd463d2485b34dad09d7
blocksan <sandyghosh555@gmail.com>
2021-04-10 15:53:40 +0530

    feat: basic folder structure created

---
+++ root-app/src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          React Root App
+      </header>
+    </div>
+  );
+}
+
+export default App;
