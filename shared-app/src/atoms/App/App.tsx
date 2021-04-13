762b84a4822b686f769eefd1f99aecf38d3bfc1d
blocksan <sandyghosh555@gmail.com>
2021-04-13 18:21:22 +0530

    feat: antd design system added with scss

--- shared-app/src/atoms/App/App.tsx
+++ shared-app/src/atoms/App/App.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
+import ButtonCom from '../Button/Button';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        React shared app
+        React shared app <ButtonCom></ButtonCom>
       </header>
     </div>
   );
---
+++ shared-app/src/atoms/Button/Button.tsx
@@ -0,0 +1,10 @@
+import  {Button} from 'antd'
+
+export default function ButtonCom() {
+    return (
+        <div>
+            <Button type="primary">Button from Antd</Button>
+        </div>
+    )
+}
+
